6e7770d07242fe2e5ae01250b3cf7d2306e9d931
Yang Fei <yangfei@yangs-mbp.wifi.local.cmu.edu>
2023-11-09 12:04:35 -0500

    finish lab10

--- src/main/java/AndrewWebServices/InMemoryDatabase.java
+++ src/main/java/AndrewWebServices/InMemoryDatabase.java
@@ -1,5 +1,8 @@
 package AndrewWebServices;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * InMemoryDatabase is a fake for the AndrewWS database which is used to improve test efficiency.
  * Remember, fakes are fully functional classes with simplified implementation.
@@ -7,6 +10,18 @@ package AndrewWebServices;
  * 
  * Hint: there are two methods you need to implement
  */
-public class InMemoryDatabase /* should there be something here? */ {
+public class InMemoryDatabase extends Database {
     // Implement your fake database here
+    private final Map<String, Integer> data = new HashMap<>();
+
+    public InMemoryDatabase() {
+        // Pre-populate the fake database with known account names and passwords
+        data.put("user1", 1234);
+        data.put("user2", 2345);
+    }
+
+    @Override
+    public int getPassword(String accountName) {
+        return data.getOrDefault(accountName, -1); // Return -1 or some "not found" value if the account does not exist
+    }
 }
--- src/test/java/AndrewWebServices/AndrewWebServicesTest.java
+++ src/test/java/AndrewWebServices/AndrewWebServicesTest.java
@@ -1,7 +1,13 @@
 package AndrewWebServices;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -26,23 +32,43 @@ public class AndrewWebServicesTest {
     public void testLogIn() {
         // This is taking way too long to test
         assertTrue(andrewWebService.logIn("Scotty", 17214));
+        database = new InMemoryDatabase();
+        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        boolean loggedIn = andrewWebService.logIn("user1", 1234);
+        assertTrue("Login should succeed with correct credentials", loggedIn);
+        loggedIn = andrewWebService.logIn("user1", 124);
+        assertFalse(loggedIn);
     }
 
     @Test
     public void testGetRecommendation() {
         // This is taking way too long to test
-        assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        // assertEquals("Animal House", andrewWebService.getRecommendation("Scotty"));
+        RecSys recommender = mock(RecSys.class);
+        when(recommender.getRecommendation("Snoopy")).thenReturn("Peanuts");
+        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        String recommendedMovie = andrewWebService.getRecommendation("Snoopy");
+        assertEquals("Peanuts", recommendedMovie);
     }
 
     @Test
     public void testSendEmail() {
         // How should we test sendEmail() when it doesn't have a return value?
         // Hint: is there something from Mockito that seems useful here?
+        promoService = mock(PromoService.class);
+        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        String email = "test@example.com";
+        andrewWebService.sendPromoEmail(email);
+        verify(promoService).mailTo(email);
     }
 
     @Test
     public void testNoSendEmail() {
-        // How should we test that no email has been sent in certain situations (like right after logging in)?
+        // How should we test that no email has been sent in certain situations (like
+        // right after logging in)?
         // Hint: is there something from Mockito that seems useful here?
+        promoService = mock(PromoService.class);
+        andrewWebService = new AndrewWebServices(database, recommender, promoService);
+        verify(promoService, never()).mailTo(anyString());
     }
 }
